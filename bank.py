ae5874f11869916044eca736894f67201a291525
Masian Parkire <parkiremasian@gmail.com>
2023-05-08 17:38:30 +0300

    added method

--- bank.py
+++ bank.py
@@ -6,7 +6,9 @@ class Account:
         self.branch=branch
     def describe_account(self):
         return f"this {self.bank} of this account number {self.accountNumber} is owned by {self.accountName}"
-    def withdraw_money():
-        return
+    def withdraw_money(self):
+        return f"I want to withdraw 5000 from account {self.accountNumber} of {self.accountName}"
+    def send_money(self):
+        return f" send money from {self.bank}{self.branch} to account number {self.accountNumber}"
             
         
--- fruit.py
+++ fruit.py
@@ -5,9 +5,9 @@ class Fruit:
         self.color=color
         self.taste=taste
     def fruit_price(self):
-        return 
+        return f"{self.name} are expensive because it has alot of {self.nutrients}"
     def describe_fruit(self):
         return f"a {self.name} is {self.taste} and it has {self.nutrients}"
     def fruit_expiry_date(self):
-        return 
+        return f"{self.name} has a {self.taste} because it has expired."
         
